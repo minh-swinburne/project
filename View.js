5c9d0cfaa4695b1bf6195386790dcced3d8b3358
Minh Nguyen <104169617@student.swin.edu.au>
2024-12-05 19:22:53 +0700

    chart type controller in view

--- View.js
+++ View.js
@@ -4,21 +4,32 @@ const View = {
         <div class="view-controller">
           <button
             v-for="dataset, index in datasets"
-            :data-index="index"
-            :class="{ active: dataset.id === this.current.id }"
+            :index="index"
+            :class="{ active: dataset.id === this.currentDs.id }"
             @click="switchDataset"
             >
             {{ dataset.name }}
           </button>
         </div>
-        <div class="view-title">
-          <h2>{{ current.title }}</h2>
-          <p>{{ current.description }}</p>
+        <div class="view-info">
+          <h2>{{ currentDs.title }}</h2>
+          <p>{{ currentDs.description }}</p>
+        </div>
+        <div class="view-charts">
+          <button
+            v-for="chart, index in currentDs.charts"
+            :index="index"
+            :chart-type="chart.type"
+            :class="{ active: chart.type === this.currentChart.type }"
+            @click="switchChart"
+            >
+            {{ this.capitalizeFirstLetter(chart.type, "-") }}
+          </button>
         </div>
         <v-chart
           :path="path"
-          :file="current.file"
-          :charts="current.charts">
+          :file="currentDs.file"
+          :charts="currentDs.charts">
         </v-chart>
       </div>
     `,
@@ -26,27 +37,50 @@ const View = {
   data() {
     return {
       datasets: [],
-      current: {
+      currentDs: {
         id: "",
+        name: "",
         title: "",
         description: "",
+        file: "",
+        charts: [],
+      },
+      currentChart: {
+        type: "",
+        key: "",
+        value: "",
       },
     };
+  },
+  mounted() {
+    this.loadJson("view.json").then((data) => {
+      this.datasets = data;
+      this.currentDs = data[0];
+      this.currentChart = this.currentDs.charts[0];
+    });
   },
   methods: {
     async loadJson(file) {
       return fetch(this.path + "/" + file).then((response) => response.json());
     },
     switchDataset(event) {
-      let idx = parseInt(event.target.getAttribute("data-index"));
-      this.current = this.datasets[idx];
-      console.log("Switching to dataset with index " + idx);
+      let index = parseInt(event.target.getAttribute("index"));
+      this.currentDs = this.datasets[index];
+      this.currentChart = this.currentDs.charts[0];
+      console.log("Switching to dataset with index " + index);
     },
+    switchChart(event) {
+      let index = parseInt(event.target.getAttribute("index"));
+      this.currentChart = this.currentDs.charts[index];
+      console.log("Switching to chart with index " + index);
+    },
+    capitalizeFirstLetter(string, separator = " ", joiner = " ") {
+      return string
+        .split(separator)
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(joiner);
+    }
   },
-  mounted() {
-    this.loadJson("view.json").then((data) => {
-      this.datasets = data;
-      this.current = data[0];
-    });
-  },
+  computed: {
+  }
 };
